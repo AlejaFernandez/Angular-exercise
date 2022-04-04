0dde934f5e903fe467ed29ae42130876d8164e24
Alejandra <alejandrafernandezg9701@gmail.com>
2022-04-04 11:00:15 -0500

    components

---
+++ src/app/section/section/section.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-section',
+  templateUrl: './section.component.html',
+  styleUrls: ['./section.component.css']
+})
+export class SectionComponent implements OnInit {
+  constructor(){
+
+  }
+  ngOnInit(): void {
+    
+
+  }
+
+  Mymodel(value: any){
+    console.log(value);
+  }
+}
